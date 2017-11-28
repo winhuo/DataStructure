d9d1beae2533377bb8693eb7ffc609e150815398
霍彧 <1262414870@qq.com>
2017-11-29 00:13:21 +0800

    2017-11-29
    修改文件，添加文档说明

--- avl_tree/avl_tree.h
+++ avl_tree/avl_tree.h
@@ -3,10 +3,10 @@ using namespace std;
 
 class AvlTreeNode
 {
-private:
-    int  key;   //jianzhi;
+public:
+    int  key;
     int  height;
-    AvlTreeNode *righChild;
+    AvlTreeNode *rightChild;
     AvlTreeNode *leftChild;
 public:
     AvlTreeNode()
@@ -16,39 +16,13 @@ public:
         this->leftChild=NULL;
         this->rightChild=NULL;
     }
-    int get_key()
-    {
-        return this->key;
-    }
-    int get_height()
-    {
-        return this->height;
-    }
-    void put_key(int k)
+    AvlTreeNode(int k,AvlTreeNode* l,AvlTreeNode *r)
     {
         this->key=k;
-    }
-    void put_height(int h)
-    {
-        this->height=h;
-    }
-    AvlTreeNode*get_leftChild()
-    {
-        return this->leftChild;
-    }
-    AvlTreeNode*get_rightChild()
-    {
-        return this->rightChild;
-    }
-    void put_rightChild(AvlTreeNode*r)
-    {
-        this->righChild=r;
-    }
-    void put_leftChild(AvlTreeNode*l)
-    {
+        this->height=0;
         this->leftChild=l;
+        this->rightChild=r;
     }
-
 
 };
 class AvlTree
@@ -60,30 +34,179 @@ public:
     {
         this->Root=NULL;
     }
-    void preOrder(AvlTreeNode n)
+
+    int height(AvlTreeNode *n)
     {
         if(n)
         {
-            cout<<n.get_key()<<"  ";
-
-        preOrder(n.get_leftChild());
-        preOrder(n.get_rightChild());
+            return n->height;
+        }
+        else
+        {
+            return 0;
         }
     }
-    void insert(int key)
+    AvlTreeNode* _leftRotation(AvlTreeNode *n)
     {
+        AvlTreeNode * n_1=n->rightChild;
+        n->rightChild=n_1->leftChild;
+        n_1->leftChild=n;
+        n->height=(height(n->leftChild)>height(n->rightChild)?height(n->leftChild):height(n->rightChild))+1;
+        n_1->height=(height(n_1->leftChild)>height(n_1->rightChild)?height(n_1->leftChild):height(n_1->rightChild))+1;
 
+        return n_1;
     }
-    void remove(int key)
+    AvlTreeNode* _rightRotation(AvlTreeNode *n)
     {
+        AvlTreeNode * n_1=n->leftChild;
+        n->leftChild=n_1->rightChild;
+        n_1->rightChild=n;
+        n->height=(height(n->leftChild)>height(n->rightChild)?height(n->leftChild):height(n->rightChild))+1;
+        n_1->height=(height(n_1->leftChild)>height(n_1->rightChild)?height(n_1->leftChild):height(n_1->rightChild))+1;
 
+        return n_1;
     }
-    void change(int key)
+    AvlTreeNode* _rightLeftRotation(AvlTreeNode *n)
     {
-
+        n->rightChild=_rightRotation(n->rightChild);
+        return _leftRotation(n);
     }
+    AvlTreeNode* _leftRightRotation(AvlTreeNode *n)
+    {
+        n->leftChild=_leftRotation(n->leftChild);
+        return _rightRotation(n);
+    }
+
+    AvlTreeNode* insert(AvlTreeNode *n,int k)
+    {
+        if(!n)
+        {
+            n=new AvlTreeNode();
+            n->key=k;
+        }
+        if(k>n->key)
+        {
+            n->rightChild=insert(n->rightChild,key);
+            if(height(n->rightChild)-height(n->leftChild)==2)
+            {
+                if(k>n->rightChild->key)
+                {
+                    n=_leftRotation(n);
+                }
+                else if(k<n->rightChild->key)
+                {
+                    n=_rightLeftRotation(n);
+                }
+            }
+        }
+        if(k<n->key)
+        {
+            n->leftChild=insert(n->leftChild,key);
+            if(height(n->leftChild)-height(n->rightChild)==2)
+            {
+                if(k>n->leftChild->key)
+                {
+                    n=_rightRotation(n);
+                }
+                else if(k<n->leftChild->key)
+                {
+                    n=_leftRightRotation(n);
+                }
+            }
+        }
+        n->height=(height(n_1->leftChild)>height(n_1->rightChild)?height(n_1->leftChild):height(n_1->rightChild))+1;
+        return n;
+    }
+    AvlTreeNode* remove(AvlTreeNode *n,int key)
+    {
+        if (!n)
+            {
+                if (key == pnode->key)
+                {
+                    if (n->leftChild!=NULL&&n->rightChild !=NULL)
+                    {
+
+                        if (height(n->leftChild) > height(n->rightChild))
+                        {
+
+                            AvlTreeNode* p = max(n->leftChild);
+                            n->key = p->key;
+                            n->leftChild = remove(n->leftChild, p->key);
+                        }
+                        else
+                        {
+                            AvlTreeNode* p = mini(n->rightChild);
+                            n->key = p->key;
+                            n->rightChild = remove(n->rightChild, ps->key);
+                        }
+
+                    }
+                    else
+                    {
+                        AvlTreeNode * p = n;
+                        if (n->leftChild != NULL)
+                            n = n->leftChild;
+                        else if (n->rightChild != NULL)
+                            n = n->rightChild;
+                        delete p;
+                        return NULL;
+                    }
+
+                }
+                else if (key > n->key)
+                {
+                    n->rightChild =  remove(n->rightChild, key);
+                    if (height(n->leftChild) - height(n->rightChild) == 2)
+                    {
+                        if (height(n->leftChild->rightChild)>height(n->leftChild->leftChild))
+                            n = _leftRightRotation(n);
+                        else
+                            n = _rightRotation(n);
+                    }
+                }
+                else if (key < n->key)
+                {
+                    n->leftChild= remove(n->leftChild, key);
+
+                    if (height(n->rightChild) - height(n->leftChild) == 2)
+                    {
+                        if (height(n->rightChild->leftChild)>height(n->rightChild->rightChild))
+                            n = _rightLeftRotation(pnode);
+                            n = _leftRotation(pnode);
+                    }
+                }
+                return pnode;
+            }
+            return nullptr;
+    }
+    AvlTreeNode* max(AvlTreeNode *n)
+    {
+        if(!n)
+        {
+            while(!n->rightChild)
+            {
+                n=n->rightChild;
+            }
+            return n;
+        }
+        return NULL;
+    }
+    AvlTreeNode* min(AvlTreeNode *n)
+    {
+        if(!n)
+        {
+            while(!n->leftChild)
+            {
+                n=n->leftChild;
+            }
+            return n;
+        }
+        return NULL;
+    }
+
     void test()
     {
 
     }
 };
+
---
+++ avl_tree/avl_tree.md
@@ -0,0 +1,46 @@
+##AVL树的插入和删除操作
+*曦剑创建于2017年11月28日*
+
+**目录 (Table of Contents)**
+
+[TOCM]
+
+
+###AVL树的节点元素
+    public:
+        int  key;  //节点的键值
+        int  height;  //节点的高度
+        AvlTreeNode *rightChild;   //指向右孩子的指针
+        AvlTreeNode *leftChild;     //指向左孩子的指针
+### AVL树中函数的作用
+*下列注释中所指的树是以传入参数作为根节点的树*
+
+
+    int height(AvlTreeNode *n)
+    //获取树的高度；
+    AvlTreeNode* _leftRotation(AvlTreeNode *n)
+    //对树进行左旋
+    AvlTreeNode* _rightRotation(AvlTreeNode *n)
+    //对树进行右旋
+    AvlTreeNode* _rightLeftRotation(AvlTreeNode *n)
+    //对树进行右左旋
+    AvlTreeNode* _leftRightRotation(AvlTreeNode *n)
+    //对树进行左右旋
+    AvlTreeNode* insert(AvlTreeNode *n,int k)
+    //对树进行插入操作
+    AvlTreeNode* remove(AvlTreeNode *n,int key)
+    //删除给定值的节点
+    AvlTreeNode* max(AvlTreeNode *n)
+    //求树中的最大元素
+    AvlTreeNode* min(AvlTreeNode *n)
+    //求树的最小节点
+    void test()
+    //测试函数
+	
+	
+	###
+
+###AVL旋转的原理图示链接
+https://www.cnblogs.com/skywang12345/p/3576969.html
+
+### End
